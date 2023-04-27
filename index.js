43bd7de1684ddd647f41727b336f2a6c4b9165f5
Dmitriy Suhin <D.Suhin94@gmail.com>
2023-04-27 22:38:17 +0300

    feat: classes for creating elemnets on page

--- index.js
+++ index.js
@@ -1,24 +1,61 @@
 class Block {
-
-    constructor(type = "div"){
-        this.elem = document.createElement(type);
-    }
-    
-    create() {
-     document.body.appendChild(this.elem);
-    }
+  constructor(type, style, parent) {
+    this.elem = document.createElement(type);
+    this.elem.classList.add(style);
+    parent.appendChild(this.elem);
+  }
 }
 
 class Textarea extends Block {
-    constructor() {
-        super("textarea");
-        this.elem.setAttribute("cols", 90);
-        this.elem.setAttribute("rows", 15);
-        this.elem.classList.add("textarea")
-    }
-    
+  constructor(style, parent) {
+    super("textarea", style, parent);
+    this.elem.setAttribute("cols", 90);
+    this.elem.setAttribute("rows", 15);
+  }
 }
 
-const textarea = new Textarea("textarea");
-textarea.create();
+class Text extends Block {
+  constructor(typeTxt, style, parent, content) {
+    super(typeTxt, style, parent);
+    this.elem.textContent = content;
+  }
+}
+
+class Btn extends Block {
+  constructor(style, parent, content) {
+    super("button", style, parent);
+    this.elem.textContent = content;
+  }
+}
+
+const btnInLine = [14, 14, 13, 13, 9];
+
+
+function createPage () {
+  const body = document.querySelector("body");
+
+  let h1 = new Text("h1", "title", body, "Rs School Virtual keyboard");
+
+  let textarea = new Textarea("textarea", body);
+
+  let keyboard = new Block("div", "keyboard", body);
+  console.log(keyboard)
+
+  keyboard = document.querySelector(".keyboard")
+  console.log(keyboard)
+
+
+  for (let i = 0; i < btnInLine.length; i++){
+    let quantity = btnInLine[i];
+    let btn_line = new Block("div", "btn_line", keyboard);
+    btn_line = document.querySelectorAll(".btn_line")[i]
+
+    for(let btn_quantity = 0; btn_quantity < quantity; btn_quantity++){
+      let btn = new Btn("btn", btn_line)
+    }
+  }
+}
+
+createPage();
+
 
